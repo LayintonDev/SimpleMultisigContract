912de721967be8fbd1aa99f59b4fba89f18f16af
LayintonDev <layintondev@gmail.com>
2024-09-11 16:54:18 +0100

    added comment to codes

--- test/Multisig.ts
+++ test/Multisig.ts
@@ -6,159 +6,173 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre, { ethers } from "hardhat";
 
+// Describe block for the Multisig contract
 describe("Multisig", function () {
 
+  // Fixture for deploying a token contract
   async function deployToken() {
+    // Get signers (accounts) and deploy the LayintonToken contract
     const [owner, otherAccount, addr1, addr2, addr3] = await hre.ethers.getSigners();
     const token = await hre.ethers.getContractFactory("LayintonToken");
     const tokenAddr = await token.deploy();
     return { tokenAddr }
   }
+
+  // Fixture for deploying the multisig contract
   async function deployMultisigContract() {
     const quorum = 3;
 
-    // Contracts are deployed using the first signer/account by default
+    // Get signers (accounts) and deploy the Multisig contract
     const [owner, otherAccount, addr1, addr2, addr3] = await hre.ethers.getSigners();
 
+    // Deploy the token contract as a prerequisite
     const { tokenAddr } = await loadFixture(deployToken);
+
+    // Deploy the multisig contract with quorum and valid signers
     const Multisig = await hre.ethers.getContractFactory("Multisig");
     const multisig = await Multisig.deploy(quorum, [addr1.address, addr2.address]);
 
     return { multisig, quorum, tokenAddr, owner, otherAccount, addr1, addr2, addr3 };
   }
 
+  // Token deployment tests
   describe("Token Deployment", function () {
+
+    // Ensure that only the owner can mint new tokens
     it("Should make sure only owner can mint", async function () {
       const [owner, otherAccount, addr1, addr2, addr3] = await hre.ethers.getSigners();
       const { tokenAddr } = await loadFixture(deployToken);
       const amount = hre.ethers.parseUnits("500", 18);
 
+      // Expect minting from a non-owner account to fail
       await expect(tokenAddr.connect(otherAccount).mint(amount)).to.be.revertedWithCustomError(tokenAddr, "NotOwner");
     });
-  })
+  });
 
+  // Token minting tests
   describe("Token Deployment", function () {
+
+    // Ensure that the correct amount of tokens are minted to the owner
     it("Should mint the right amount to the owner address", async function () {
       const [owner, otherAccount, addr1, addr2, addr3] = await hre.ethers.getSigners();
       const { tokenAddr } = await loadFixture(deployToken);
       const amount = hre.ethers.parseUnits("100000", 1);
 
-      await expect(tokenAddr.mint(100000)).to.emit(tokenAddr, "MintSuccessful") // tokenAddr.mint(amount);
+      // Expect the mint event to be emitted successfully
+      await expect(tokenAddr.mint(100000)).to.emit(tokenAddr, "MintSuccessful");
 
+      // Check the owner's balance is correctly updated
       expect(await tokenAddr.balanceOf(owner.address)).to.equal(hre.ethers.parseUnits("600000", 18));
-
     });
-  })
+  });
 
+  // Multisig deployment tests
   describe("Deployment", function () {
+
+    // Ensure the correct quorum number is set
     it("Should set the right quorum number", async function () {
       const [owner, otherAccount, addr1, addr2, addr3] = await hre.ethers.getSigners();
       const { multisig } = await loadFixture(deployMultisigContract);
 
+      // Check that the quorum is 3 as expected
       expect(await multisig.quorum()).to.equal(3);
     });
-    it("Should set the rigth number of valid signers", async function () {
+
+    // Ensure the correct number of valid signers is set
+    it("Should set the right number of valid signers", async function () {
       const [owner, otherAccount, addr1, addr2, addr3] = await hre.ethers.getSigners();
       const { multisig } = await loadFixture(deployMultisigContract);
 
+      // Check that the number of valid signers is 3
       expect(await multisig.noOfValidSigners()).to.equal(3);
     });
 
+    // Ensure the owner is added to the valid signers if not done at deployment
     it("Should add owner to valid signer if not added at deployment", async function () {
       const [owner, otherAccount, addr1, addr2, addr3] = await hre.ethers.getSigners();
       const { multisig } = await loadFixture(deployMultisigContract);
 
+      // Check that the owner is a valid signer
       expect(await multisig.getAVlaidSigner(owner.address)).to.equal(true);
     });
-
   });
 
-  describe("Tranfer", function () {
-    it("Should revert if the sender is not a valid signer", async function () {
+  // Token transfer tests
+  describe("Transfer", function () {
 
+    // Ensure a transfer fails if the sender is not a valid signer
+    it("Should revert if the sender is not a valid signer", async function () {
       const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
       const amount = hre.ethers.parseUnits("20", 18);
 
+      // Expect transfer to fail if the sender is not a valid signer
       await expect(multisig.connect(addr3).transfer(amount, addr1.address, tokenAddr)).to.revertedWith("invalid signer");
     });
 
-    it("Should set the rigth number of valid signers", async function () {
-      const [owner, otherAccount, addr1, addr2, addr3] = await hre.ethers.getSigners();
-      const { multisig } = await loadFixture(deployMultisigContract);
-
-      expect(await multisig.noOfValidSigners()).to.equal(3);
-    });
+    // Ensure transfer fails when zero amount is sent
     it("Should revert when zero amount is sent", async function () {
       const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
       const amount = hre.ethers.parseUnits("0", 18);
 
+      // Expect transfer to fail if the amount is zero
       await expect(multisig.transfer(amount, addr1.address, tokenAddr)).to.revertedWith("can't send zero amount");
     });
-    // it("Should revert if senders address is zero", async function () {
-    //   const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
 
-    //   await tokenAddr.transfer(multisig, hre.ethers.parseUnits("500", 18));
-    //   const amount = hre.ethers.parseUnits("10", 18);
-
-
-    //   const zeroAddress = "0x0000000000000000000000000000000000000000";
-
-    //   // Attempt a low-level call with the zero address
-    //   await expect(
-    //     ethers.provider.send("eth_sendTransaction", [{
-    //       from: ethers.ze,
-    //       to: multisig,
-    //       data: multisig.interface.encodeFunctionData("transfer", [amount, addr1.address, tokenAddr]),
-
-    //     }])
-    //   ).to.be.revertedWith("address zero found");
-
-    //   // await hre.ethers.provider.send("hardhat_setSender", ["0x0000000000000000000000000000000000000000"]); //set sender address to zero
-
-    //   // await expect(multisig.transfer(amount, addr1.address, tokenAddr)).to.revertedWith("address zero found");
-    // });
-    it("Should revert if receipient address is zero", async function () {
+    // Ensure transfer fails if recipient address is zero
+    it("Should revert if recipient address is zero", async function () {
       const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
       const amount = hre.ethers.parseUnits("0", 18);
 
+      // Expect transfer to fail if the recipient address is zero
       await expect(multisig.transfer(amount, hre.ethers.ZeroAddress, tokenAddr)).to.revertedWith("can't send zero amount");
     });
+
+    // Ensure transfer fails if token address is zero
     it("Should revert if token address is zero", async function () {
       const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
       await tokenAddr.transfer(multisig, hre.ethers.parseUnits("500", 18));
       const amount = hre.ethers.parseUnits("10", 18);
 
+      // Expect transfer to fail if the token address is zero
       await expect(multisig.transfer(amount, addr2.address, hre.ethers.ZeroAddress)).to.revertedWith("address zero found");
     });
+
+    // Ensure transfer fails if the multisig balance is less than the transfer amount
     it("Should revert if token address balance is less than amount", async function () {
       const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
       await tokenAddr.transfer(multisig, hre.ethers.parseUnits("500", 18));
       const amount = hre.ethers.parseUnits("600", 18);
 
+      // Expect transfer to fail if there are insufficient funds
       await expect(multisig.transfer(amount, addr2.address, tokenAddr)).to.revertedWith("insufficient funds");
     });
 
+    // Ensure transaction is submitted correctly
     it("Should check if transaction is submitted correctly", async function () {
       const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
       await tokenAddr.transfer(multisig, hre.ethers.parseUnits("500", 18));
       const amount = hre.ethers.parseUnits("20", 18);
 
+      // Perform the transfer
       await multisig.transfer(amount, addr1.address, tokenAddr);
 
+      // Ensure the transaction count has increased
       expect(await multisig.txCount()).to.equal(1);
-
-
     });
   });
 
+  // Approving transactions tests
   describe("Approve Trx", function () {
+
+    // Ensure approval fails if an invalid transaction ID is passed
     it("Should revert if invalid trx id is passed", async function () {
       const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
 
-
+      // Expect approval to fail if the transaction ID is invalid
       await expect(multisig.approveTx(0)).to.revertedWith("invalid tx id");
-
     });
+
+    // Additional tests for transaction approvals (not provided)...
     it("Should revert if token address balance is less than amount", async function () {
       const { multisig, tokenAddr, addr1, addr2, addr3 } = await loadFixture(deployMultisigContract);
       await tokenAddr.transfer(multisig, hre.ethers.parseUnits("500", 18));
